6ebff08e5b03dcb78d06ed6e7a07741001090719
muhdsahal <sahalshalu830@gmail.com>
2025-01-23 18:20:15 +0530

    first commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,19 @@
+import { Typography } from "@material-tailwind/react";
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div className="flex justify-center items-center">
+        <img className=" w-1/3" src="/glovista_logo.svg" alt="logo" />
+      </div>
+      
+
+      <Typography className="text-7xl font-semibold text-center text-black">
+        Under Construction
+      </Typography>
+    </div>
+  );
+}
+
+export default Home;
